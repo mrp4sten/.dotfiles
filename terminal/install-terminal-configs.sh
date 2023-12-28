4de08182e9dbb8b9c0b987a23c9b103fc672892e
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-12-27 18:49:31 -0600

    Update terminal configuration script

--- terminal/install-terminal-configs.sh
+++ terminal/install-terminal-configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author: Mauricio Pasten (mrp4sten)
 
-TERMINAL_DIR=.dotfiles/terminal
+TERMINAL_DIR=~/.dotfiles/terminal
 
 # nano
 sudo pacman -Sy nano
@@ -35,24 +35,24 @@ ln -s -f ${TERMINAL_DIR}/.tmux.conf ~/
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 pacman -S bat
-cd ~
+cd ~ || exit
 git clone git@github.com:catppuccin/bat.git
 mkdir -p "$(bat --config-dir)/themes"
-cd bat
+cd bat || exit
 cp *.tmTheme "$(bat --config-dir)/themes"
 bat cache --build
 pacman -S lsd
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 paru -S ranger
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+echo "default_linemode devicons" >>"$HOME"/.config/ranger/rc.conf
 paru -S ueberzugpp
-echo "set preview_images true" >> $HOME/.config/ranger/rc.conf
-echo "set preview_images_method ueberzug" >> $HOME/.config/ranger/rc.conf
+echo "set preview_images true" >>"$HOME"/.config/ranger/rc.conf
+echo "set preview_images_method ueberzug" >>"$HOME"/.config/ranger/rc.conf
 paru -S pass
 cp ${TERMINAL_DIR}/zsh/.zshrc ~/
 cp ${TERMINAL_DIR}/zsh/.p10k.zsh ~/
