4cfc66cad538911babf6fc75b516171c9daf9428
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-01-11 12:00:56 -0600

    Update exports.sh script

--- terminal/zsh/_exports/exports.sh
+++ terminal/zsh/_exports/exports.sh
@@ -1,3 +1,1 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/home/mrp4sten/NicheAimWorkspace/utils/credentials/nicheaim-labs-c7ce73ed880d.json"
-# export JAVA_HOME="/usr/lib/jvm/java-21-openjdk"
-# export M2_HOME="/opt/maven"
+#!/bin/bash
