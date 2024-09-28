ad68c4006d3ff27cd44c8c9a33c8646a30a2aafd
mavor <mauricio.pasten.martinez@gmail.com>
2024-09-28 17:47:27 -0600

    updating settings and icons for better size

--- wm/suckless/dwm/config.def.h
+++ wm/suckless/dwm/config.def.h
@@ -18,8 +18,8 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1;                     /* 0 means no systray */
 static const int showbar = 1;                         /* 0 means no bar */
 static const int topbar = 1; /* 0 means bottom bar */ /* 0 means bottom bar */
-static const char *fonts[] = {"Hack Nerd Font Mono:size=16"};
-static const char dmenufont[] = "Hack Nerd Font Mono:size=21";
+static const char *fonts[] = {"Hack Nerd Font Mono:size=18"};
+static const char dmenufont[] = "Hack Nerd Font Mono:size=18";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
