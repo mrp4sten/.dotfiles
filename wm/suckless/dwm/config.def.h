d179c576209293ce7ea9b558e85d1e626b4e3b6b
mavor <mauricio.pasten.martinez@gmail.com>
2024-09-28 18:12:02 -0600

    updating color scheme

--- wm/suckless/dwm/config.def.h
+++ wm/suckless/dwm/config.def.h
@@ -25,15 +25,15 @@ static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
-/* solarized colors http://ethanschoonover.com/solarized */
-static const char s_base03[] = "#002b36";
-static const char s_base02[] = "#073642";
-static const char s_base01[] = "#586e75";
-static const char s_base00[] = "#657b83";
-static const char s_base0[] = "#839496";
-static const char s_base1[] = "#93a1a1";
-static const char s_base2[] = "#eee8d5";
-static const char s_base3[] = "#fdf6e3";
+/* red dark custom theme */
+static const char s_base03[] = "#360006";
+static const char s_base02[] = "#420707";
+static const char s_base01[] = "#755858";
+static const char s_base00[] = "#836565";
+static const char s_base0[] = "#968383";
+static const char s_base1[] = "#a19393";
+static const char s_base2[] = "#eed5d5";
+static const char s_base3[] = "#fde3e3";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     {s_base0, s_base03, s_base2},      /* SchemeNorm dark */
