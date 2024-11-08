c3318246ef92247b4a30976ea80f2471545da99f
mavor <mauricio.pasten.martinez@gmail.com>
2024-11-07 22:50:07 -0600

    adding gitignore and adding keyboardlayout to dwmblocks

--- wm/suckless/dwmblocks/blocks.h
+++ wm/suckless/dwmblocks/blocks.h
@@ -1,11 +1,12 @@
 static const Block blocks[] = {
     //   Icon    Command                          Update Interval     Update
     //   Signal
-    {"  ", "checkupdates | wc -l", 60, 0},
-    {"", "brightness", 2, 0},
-    {"", "volume", 2, 0},
+    // {"  ", "checkupdates | wc -l", 1, 0},
+    {"󰌌  ", "xkblayout-state print '%s'", 2, 0},
+    {"", "brightness", 1, 0},
+    {"", "volume", 1, 0},
     {"", "battery", 60, 0},
-    {"", "date '+ %d/%m/%Y   %H:%M%p'", 5, 0},
+    {"", "date '+ %d/%m/%Y   %H:%M%p'", 1, 0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no
