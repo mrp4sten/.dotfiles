0b6ef964d07bc4f4b2811e3dc5623802159ad116
mavor <mauricio.pasten.martinez@gmail.com>
2024-11-07 20:24:49 -0600

    adding and changing default wallpaper

--- wm/suckless/dwm/scripts/autostart.sh
+++ wm/suckless/dwm/scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Mauricio Pasten (mavor)
 # mauricio.pasten.martinez@gmail.com
-feh --bg-fill ~/.dotfiles/wm/suckless/dwm/wallpapers/yone_1.jpg
+feh --bg-fill ~/.dotfiles/wm/suckless/dwm/wallpapers/matrix-01.jpg
 dwm &
 dwmblocks &
 picom --config ~/.dotfiles/wm/suckless/picom/picom.conf &
