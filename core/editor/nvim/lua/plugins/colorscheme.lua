7e61589f9b4a887a5602d93f9d6e6790db9acc7a
mrp4sten <mauricio.pasten.martinez@gmail.com>
2026-02-19 19:37:47 -0600

    feat(nvim): enable transparent mode for gruvbox colorscheme

--- core/editor/nvim/lua/plugins/colorscheme.lua
+++ core/editor/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
