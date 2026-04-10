a5839ab7b1e6aab425350a7ea311a4796674737b
mrp4sten <mauricio.pasten@proton.me>
2026-04-09 21:55:36 -0600

    adding hyprland setup

---
+++ system/desktop/hypr/scripts/screensharing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: mrp4sten
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
